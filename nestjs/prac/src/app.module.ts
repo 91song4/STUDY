ee1ae5b09e4b7cc10e9937c3ef86b097aeb5579e
91song4 <91song4@daum.net>
2023-02-26 17:58:45 +0900

    NestJS: 레파지토리 연결

--- nestjs/prac/src/app.module.ts
+++ nestjs/prac/src/app.module.ts
@@ -1,9 +1,18 @@
+import { TypeOrmConfigService } from './config/typeorm.config.service';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { BoardModule } from './board/board.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmConfigService,
+    }),
+    BoardModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
