a16ecea75c3e10db3a73eef5a68ee6a31160a64b
91song4 <91song4@daum.net>
2023-04-14 20:18:37 +0900

    동기와 비동기 코드 실행방식

---
+++ data_structure/src/queue.ts
@@ -0,0 +1,14 @@
+export class Queue {
+  private readonly queue: number[] | string[];
+  private front: number;
+  private rear: number;
+
+  constructor(private readonly maxSize: number) {
+    this.queue = new Array(maxSize);
+    this.front = -1;
+    this.rear = -1;
+  }
+
+  // 어떤상태가 비어있음일까?
+  isEmpty() {}
+}
