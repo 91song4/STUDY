c556dfa1e4e73e560bc122e8fbecd2e54f733c65
91song4 <91song4@daum.net>
2023-07-14 02:38:03 +0900

    docs: 게시글 전체 가져오기 swagger 추가

--- nestjs/prac-board2/src/boards/boards.controller.ts
+++ nestjs/prac-board2/src/boards/boards.controller.ts
@@ -1,11 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board } from './board.entity';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('boards')
 @Controller('boards')
 export class BoardsController {
   constructor(private readonly boardsService: BoardsService) {}
 
+  /**
+   * 게시글 전체 가져오기
+   */
   @Get()
   async getAllBoards(): Promise<Board[]> {
     return this.boardsService.getAllBoards();
