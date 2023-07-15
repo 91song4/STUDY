ca2e23b279ac8a0c5374dabe23c6db1936871ed2
91song4 <91song4@daum.net>
2023-07-16 02:58:49 +0900

    feat: IsNotEmpty Validation 추가

--- nestjs/prac-board2/src/boards/dto/create-board.dto.ts
+++ nestjs/prac-board2/src/boards/dto/create-board.dto.ts
@@ -1,10 +1,11 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateBoardDTO {
   /**
    * 제목
    * @example "커피가 마시고 싶다..!"
    */
+  @IsNotEmpty({ message: '빈 값이 들어있습니다' })
   @IsString()
   title: string;
 
@@ -12,6 +13,7 @@ export class CreateBoardDTO {
    * 내용
    * @example "메가리카노!!!"
    */
+  @IsNotEmpty({ message: '빈 값이 들어있습니다' })
   @IsString()
   description: string;
 }
